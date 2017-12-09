e0dafdd4df34ba9109551792516159a808d7d932
Moriah Rahamim <moriahfaye@gmail.com>
2017-12-09 15:32:53 -0500

    added support for Visa and MasterCard
    * put all card rules into an object for scalability
    * added support for different-length prefixes

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,19 +7,50 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
+// Return true if any of the prefixes are found at the beginning of cardNumber
+var prefixMatch = function(prefixes, cardNumber){
+
+  for( let i = 0; i < prefixes.length; i++ ) {
+    if(cardNumber.indexOf(prefixes[i]) === 0){
+      return true;
+    }
+  }
+
+  return false;
+}
 
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  var prefix = cardNumber.slice(0, 2);
-  var length = cardNumber.length;
-
-  if((prefix === '38' || prefix === '39') && length === 14){
-    return 'Diner\'s Club';
-  } else if ((prefix === '34' || prefix === '37') && length === 15){
-    return 'American Express';
+  var networks = {
+    'American Express': {
+      prefixes: ['34', '37'],
+      lengths: [15]
+    },
+    'Diner\'s Club': {
+      prefixes: ['38', '39'],
+      lengths: [14]
+    },
+    Visa: {
+      prefixes: ['4'],
+      lengths: [13, 16, 19]
+    },
+    MasterCard: {
+      prefixes: ['51', '52', '53', '54', '55'],
+      lengths: [16]
+    }
   }
-  return false;
+
+  for ( let network in networks) {
+    let prefixes = networks[network].prefixes;
+    let lengths = networks[network].lengths;
+
+    if ( prefixMatch(prefixes, cardNumber) && lengths.indexOf(cardNumber.length) >= 0 ){
+      return network;
+    }
+  }
+
+  return undefined;
 };
