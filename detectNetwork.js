23e7ad537ecabc1c2c98628762555308ecdfeea9
Moriah Rahamim <moriahfaye@gmail.com>
2017-12-09 15:26:56 -0500

    allow program to detect Diner's Club and American Express

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,12 +7,19 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
+
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  var prefix = cardNumber.slice(0, 2);
+  var length = cardNumber.length;
+
+  if((prefix === '38' || prefix === '39') && length === 14){
+    return 'Diner\'s Club';
+  } else if ((prefix === '34' || prefix === '37') && length === 15){
+    return 'American Express';
+  }
+  return false;
 };
-
-
